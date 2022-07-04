ba236e4f4fea33b5d205df37043b818d691abc7a
Leo.Teixeira <leo182tss@gmail.com>
2022-07-04 00:18:06 -0300

    Exercicio 1

---
+++ NumerosNaturais/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um número: ");
+        int numero = Int32.Parse(Console.ReadLine());
+        int valorToal= 0;
+        for (int i=0;i<=numero;i++)
+        {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    valorToal = valorToal + i;
+                }
+        }
+        Console.WriteLine("A soma dos números naturais múltiplos de 3 e 5 é igual a " + valorToal);   
+    }
+    
+}
+
+
+
